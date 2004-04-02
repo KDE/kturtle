18ec8da3517245bbaf787cf0d9a49290b9cc928b
Anne-Marie Mahfouf <annma@kde.org>
2004-04-02 03:08:11 +0000

    update TODO
    svn path=/trunk/kdeedu/kturtle/; revision=300571

--- src/kturtle.h
+++ src/kturtle.h
@@ -14,10 +14,6 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
- 
-/**
- * This file declares a class for  KTurtle main window. It provides geometry management, menubar, toolbar, statusbar, the editor and *the canvas. The editor is a KTextEditor part and the canvas is derived from its own class.
- **/
 
 // BEGIN include and defines 
 
@@ -49,132 +45,146 @@
 
 class KRecentFilesAction;
 
+/**
+ *This file declares a class for  KTurtle main window. It provides geometry management, menubar, toolbar, statusbar, the editor and *the canvas. The editor is a KTextEditor part and the canvas is derived from its own class
+ *
+ * @short Main window class
+ * @author Cies Breijs <cies # showroommama ! nl>
+ * @version 0.1
+ */
+ 
 class MainWindow : public KParts::MainWindow
-{   Q_OBJECT
+{   
+
+ Q_OBJECT
 
   public:
-    MainWindow(KTextEditor::Document* = 0L);
-    virtual ~MainWindow();
+  	/**
+	Constructor
+	@param KTextEditor::Document
+	*/
+    	MainWindow(KTextEditor::Document* = 0L);
+    	virtual ~MainWindow();
   
   signals:
-    void ResizeCanvas(int x, int y);
+    	void ResizeCanvas(int x, int y);
 
   private:
-    // init'ed in contructor
-    Canvas             *TurtleView;
-    QWidget            *BaseWidget;
-    QGridLayout        *BaseLayout;
-    QDockWindow        *EditorDock;
-    QString             CurrentFile;
-    QString             filename2saveAs;
-          
-    // setup's (actions, editor, statusbar, canvas)
-    void setupActions();
-      KAction            *run;
-      KAction            *stop;
-      KAction            *openExAction;
-      KAction            *openFileAction;
-      KAction            *newAction;
-      KToggleAction      *m_fullscreen;
-      KToggleAction      *colorpicker;
-      // KToggleAction      *showEditor; // DEPPRICATED
-      KRecentFilesAction *m_recentFiles;
-      KAction            *ContextHelp;
-      bool                b_fullscreen:1;
-      bool                b_editorShown:1;
-    void setupEditor();
-      KTextEditor::View           *view() const { return editor; }
-      KTextEditor::EditInterface  *ei;    
-      KTextEditor::Document       *doc;
-      KTextEditor::View           *editor;
-    void setupCanvas();
-    void setupStatusBar();
-
-    // configuration related
-    void readConfig(KConfig *config);
-    void saveSettings(KConfig *config);
-    QWidget            *general;
-    QWidget            *language;
-    QGroupBox          *WidthHeightBox;
-    QLabel             *WidthLabel;
-    QLabel             *HeightLabel;
-    KIntNumInput       *kcfg_CanvasWidth;
-    KIntNumInput       *kcfg_CanvasHeight;
-    KComboBox          *kcfg_LanguageComboBox;
-    QLabel             *LanguageLabel;
-    ColorPicker        *picker;
-    QString             helpKeyword;
-    
-    // run related
-    void startExecution();
-    void finishExecution();
-    Executer           *exe;
-    bool                allreadyError;
-    bool                executing;
-
-    // fullscreen related
-    void updateFullScreen();
+	// init'ed in contructor
+	Canvas             *TurtleView;
+	QWidget            *BaseWidget;
+	QGridLayout        *BaseLayout;
+	QDockWindow        *EditorDock;
+	QString             CurrentFile;
+	QString             filename2saveAs;
+		
+	// setup's (actions, editor, statusbar, canvas)
+	void setupActions();
+	KAction            *run;
+	KAction            *stop;
+	KAction            *openExAction;
+	KAction            *openFileAction;
+	KAction            *newAction;
+	KToggleAction      *m_fullscreen;
+	KToggleAction      *colorpicker;
+	// KToggleAction      *showEditor; // DEPPRICATED
+	KRecentFilesAction *m_recentFiles;
+	KAction            *ContextHelp;
+	bool                b_fullscreen:1;
+	bool                b_editorShown:1;
+	void setupEditor();
+	KTextEditor::View           *view() const { return editor; }
+	KTextEditor::EditInterface  *ei;    
+	KTextEditor::Document       *doc;
+	KTextEditor::View           *editor;
+	void setupCanvas();
+	void setupStatusBar();
+	
+	// configuration related
+	void readConfig(KConfig *config);
+	void saveSettings(KConfig *config);
+	QWidget            *general;
+	QWidget            *language;
+	QGroupBox          *WidthHeightBox;
+	QLabel             *WidthLabel;
+	QLabel             *HeightLabel;
+	KIntNumInput       *kcfg_CanvasWidth;
+	KIntNumInput       *kcfg_CanvasHeight;
+	KComboBox          *kcfg_LanguageComboBox;
+	QLabel             *LanguageLabel;
+	ColorPicker        *picker;
+	QString             helpKeyword;
+	
+	// run related
+	void startExecution();
+	void finishExecution();
+	Executer           *exe;
+	bool                allreadyError;
+	bool                executing;
+	
+	// fullscreen related
+	void updateFullScreen();
     
   private slots:
-    void slotNewFile();
-    void slotOpenFile();
-    void loadFile(KURL url);
-    void slotOpen(const KURL&);
-    void slotOpenEx();
-    void slotSaveFile();
-    void slotSaveAs();
-    void slotSave(KURL &);
-    void slotSaveCanvas();
-    void slotPrint();
-    void slotQuit();
-    
-    void slotExecute();
-    void slotAbortExecution();
-    void slotErrorDialog(QString msg, uint row = 0, uint col = 0, uint code = 0);
-    void slotInputDialog(QString& value);
-    void slotMessageDialog(QString text);
-    
-    void slotEditor();
-    void slotShowEditor();
-    void slotSetHighlightstyle(QString langCode);   
-    void slotUndo();
-    void slotRedo();
-    void slotCut();
-    void slotCopy();
-    void slotPaste();
-    void slotSelectAll();
-    void slotClearSelection();
-    void slotFind();
-    void slotFindNext();
-    void slotFindPrevious();
-    void slotReplace();
-    void slotToggleInsert();
-    void slotInsertText(QString);
-    void slotToggleLineNumbers();
-    void slotIndent();
-    void slotUnIndent();
-    void slotCleanIndent();
-    void slotComment();
-    void slotUnComment();
-    
-    void slotSettings();
-    void slotUpdateSettings();
-    void slotConfigureToolbars();
-    void slotConfigureKeys();
-    
-    void slotContextHelp();
-    void slotContextHelpUpdate();
-
-    void slotStatusBar(QString text, int place);
-    void slotCursorStatusBar();
-    
-    void slotUpdateCanvas();
-    void slotColorPicker();
-
-    void slotToggleFullscreen();
+	void slotNewFile();
+	void slotOpenFile();
+	void loadFile(KURL url);
+	void slotOpen(const KURL&);
+	void slotOpenEx();
+	void slotSaveFile();
+	void slotSaveAs();
+	void slotSave(KURL &);
+	void slotSaveCanvas();
+	void slotPrint();
+	void slotQuit();
+	
+	void slotExecute();
+	void slotAbortExecution();
+	void slotErrorDialog(QString msg, uint row = 0, uint col = 0, uint code = 0);
+	void slotInputDialog(QString& value);
+	void slotMessageDialog(QString text);
+	
+	void slotEditor();
+	void slotShowEditor();
+	void slotSetHighlightstyle(QString langCode);   
+	void slotUndo();
+	void slotRedo();
+	void slotCut();
+	void slotCopy();
+	void slotPaste();
+	void slotSelectAll();
+	void slotClearSelection();
+	void slotFind();
+	void slotFindNext();
+	void slotFindPrevious();
+	void slotReplace();
+	void slotToggleInsert();
+	void slotInsertText(QString);
+	void slotToggleLineNumbers();
+	void slotIndent();
+	void slotUnIndent();
+	void slotCleanIndent();
+	void slotComment();
+	void slotUnComment();
+	
+	void slotSettings();
+	void slotUpdateSettings();
+	void slotConfigureToolbars();
+	void slotConfigureKeys();
+	
+	void slotContextHelp();
+	void slotContextHelpUpdate();
+	
+	void slotStatusBar(QString text, int place);
+	void slotCursorStatusBar();
+	
+	void slotUpdateCanvas();
+	void slotColorPicker();
+	
+	void slotToggleFullscreen();
 
   protected:
-    virtual bool event(QEvent* e);
+    	virtual bool event(QEvent* e);
 };
 
 
