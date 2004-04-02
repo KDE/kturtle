98318fd0afbdd199dd1e571bcf03ba3c73616b52
Anne-Marie Mahfouf <annma@kde.org>
2004-04-02 02:40:35 +0000

    cosmetic fixes: licences + indent
    svn path=/trunk/kdeedu/kturtle/; revision=300567

--- src/canvas.cpp
+++ src/canvas.cpp
@@ -1,6 +1,20 @@
 /*
  * Copyright (C) 2003 Cies Breijs <cies # showroommama ! nl>
+ 
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of version 2 of the GNU General Public
+    License as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
+
 
 #include <qpainter.h>
 #include <kdebug.h>
@@ -12,8 +26,6 @@
 
 const float PI=3.14159265358979323846;
 
-// Implementation of the canvas //
-//
 Canvas::Canvas(QWidget *parent, const char *name) : QCanvasView(0, parent, name) {
     // Create a new canvas for this view
     TurtleCanvas = new QCanvas(parent);
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,3 +1,25 @@
+/*=============================================================================
+author        :Walter Schreppers
+filename      :lexer.h
+description   :Split an input stream up into tokens, eat up white space and
+               comments and keep track of row and column
+bugreport(log):column will not be incremented enough when numbers are read
+=============================================================================*/
+/*
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of version 2 of the GNU General Public
+    License as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+ 
 #include <qdom.h>
 #include <qfile.h>
 
