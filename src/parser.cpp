2798aa0150fbd00122dd69bacc3593537652e1ea
Anne-Marie Mahfouf <annma@kde.org>
2004-08-30 20:16:49 +0000

    keep the copyrights
    svn path=/trunk/kdeedu/kturtle/; revision=342799

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,4 +1,7 @@
 /*
+     Copyright (C) 2003 by Walter Schreppers 
+     Copyright (C) 2004 by Cies Breijs   
+     
     This program is free software; you can redistribute it and/or
     modify it under the terms of version 2 of the GNU General Public
     License as published by the Free Software Foundation.
