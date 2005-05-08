934618cddee22f4c9ef784d396aa1abbe38b44bc
Anne-Marie Mahfouf <annma@kde.org>
2005-05-08 23:17:21 +0000

    fix typo
    svn path=/trunk/KDE/kdeedu/doc/kturtle/; revision=411341

--- src/main.cpp
+++ src/main.cpp
@@ -83,7 +83,7 @@ int main(int argc, char **argv)
 			I18N_NOOP("Brazilian Portuguese Data Files"),
 			"riverson # ccv.ufc.br");
         about.addCredit("Karl Ove Hufthammer",
-			 I18N_NOOP("Norwegian Nynorsk and Bookmal Data files"), "karl # huftis.org");
+			 I18N_NOOP("Norwegian Nynorsk and Bokmål Data files"), "karl # huftis.org");
 	about.addCredit("Albert Astals Cid",
 	      I18N_NOOP("Parser Cyrillic support"),
 	                "astals11 # terra.es");
