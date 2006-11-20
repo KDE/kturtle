111d4ad83fd3ca59236cabb77a28183eccd4fc33
Albert Astals Cid <tsdgeos@terra.es>
2006-11-20 18:32:55 +0000

    -       Copyright (C) 2003-2006 Cies Breijs <cies # kde ! nl> +       Copyright (C) 2003-2006 Cies Breijs <cies AT kde DOT nl> to please krazy, cies agrees with the change CCMAIL: cies.breijs@gmail.com
    svn path=/trunk/KDE/kdeedu/kturtle/; revision=606541

--- src/interpreter/tokenizer.cpp
+++ src/interpreter/tokenizer.cpp
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2003-2006 Cies Breijs <cies # kde ! nl>
+	Copyright (C) 2003-2006 Cies Breijs <cies AT kde DOT nl>
 
 	This program is free software; you can redistribute it and/or
 	modify it under the terms of version 2 of the GNU General Public
