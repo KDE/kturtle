a9528f7be95c4c277e7118f5af2e6b22022ef263
Niels Slot <nielsslot@gmail.com>
2009-01-30 21:14:47 +0000

    Some small code formatting cleanup.
    svn path=/trunk/KDE/kdeedu/kturtle/; revision=918858

--- src/interpreter/definitions.rb
+++ src/interpreter/definitions.rb
@@ -626,12 +626,10 @@ new_item()
 	double endCondition   = node->child(2)->value()->number();
 	double step           = node->child(3)->value()->number();
 
-	if (
- 	(startCondition < endCondition && currentCount + step <= endCondition) ||
-	    (startCondition > endCondition && currentCount + step >= endCondition && step<0) //negative loop sanity check, is it implemented?
-		|| (startCondition==endCondition && firstIteration) // for expressions like for $n=1 to 1
-	   ) {
-	if (!firstIteration)
+	if ((startCondition < endCondition && currentCount + step <= endCondition) ||
+	    (startCondition > endCondition && currentCount + step >= endCondition && step<0) ||  //negative loop sanity check, is it implemented?
+	    (startCondition ==endCondition && firstIteration) ) { // for expressions like for $n=1 to 1
+		if (!firstIteration)
 			(*currentVariableTable())[node->child(0)->token()->look()].setNumber(currentCount + step);
 		newScope = node->child(4); // (re-)execute the scope
 	} else {
