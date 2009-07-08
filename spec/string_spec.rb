3d9cb4fc956ca4025eab9440ac1aad02192c1dfb
Niels Slot <nielsslot@gmail.com>
2009-07-08 13:53:17 +0000

    Some specs for strings in KTurtle. I'm still missing a unicode test, since I don't know any good example of a unicode character.
    svn path=/trunk/KDE/kdeedu/kturtle/; revision=993330

--- spec/string_spec.rb
+++ spec/string_spec.rb
@@ -2,9 +2,49 @@ require File.dirname(__FILE__) + '/spec_helper.rb'
 $i = Interpreter.instance
 
 describe "string" do
-  it "should work as expected with simple strings"
+  it "should work as expected with simple strings" do
+    $i.should_run_clean <<-EOS
+      $x = "test"
+      $y = "kturtle"
+      assert $x == "test"
+      assert $y == "kturtle"
+
+      $cies = "Cies"
+      $author = "Cies"
+      assert $cies == $author
+    EOS
+  end
+
   it "should work as expected with unicode characters"
-  it "should work as expected with escaped quotes"
-  it "should allow string concatenation"
-  it "should produce an error when not properly terminated"
+
+  it "should work as expected with escaped quotes" do
+    $i.should_run_clean <<-EOS
+      $x = "\\"quote"
+      $y = "\\"quotes\\""
+
+      # We currently have no way to validate if the string 
+      #  was correctly parsed by KTurtle. e.g. printing a 
+      #	 string with quotes also prints the slashes.
+
+      assert false
+    EOS
+  end
+
+  it "should allow string concatenation" do
+    $i.should_run_clean <<-EOS
+      $x = "K"
+      $y = "Turtle"
+      $z = $x + $y
+      assert $z == "KTurtle"
+
+      $first = "Cies"
+      $last = "Breijs"
+      $name = $first + " " + $last
+      assert $name == "Cies Breijs"
+    EOS
+  end
+
+  it "should produce an error when not properly terminated" do
+    $i.run("\"test").errors?.should be_true
+  end
 end
