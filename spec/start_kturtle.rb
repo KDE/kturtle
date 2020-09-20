db56ac490bfe034caa64b9c4b08411830731a17f
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2020-09-20 10:32:18 +0200

    Convert license statements to SPDX expressions

--- spec/start_kturtle.rb
+++ spec/start_kturtle.rb
@@ -1,21 +1,8 @@
 #!/usr/bin/env ruby
-#  Copyright (C) 2009 by Cies Breijs
-#  Copyright (C) 2009 by Niels Slot
+#  SPDX-FileCopyrightText: 2009 Cies Breijs
+#  SPDX-FileCopyrightText: 2009 Niels Slot
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public
-#  License as published by the Free Software Foundation; either
-#  version 2 of the License, or (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public
-#  License along with this program; if not, write to the Free
-#  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-#  Boston, MA 02110-1301, USA.
+#  SPDX-License-Identifier: GPL-2.0-or-later
 
 puts "Staring the KTurtle interpreter in DBUS mode..."
 k = IO.popen './src/kturtle --dbus'
